5c1604d8248e9518e82aaee85e1074a4b2c4076e
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-19 21:54:24 +0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by aplle on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
